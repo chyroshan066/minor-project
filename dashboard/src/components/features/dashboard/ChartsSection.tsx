6912237dcb5d59b7d6fd82852da57c71a144542a
unknown <chyroshan066@gmail.com>
2026-04-02 11:48:52 +0545

    commented activeusersChart

--- dashboard/src/components/features/dashboard/ChartsSection.tsx
+++ dashboard/src/components/features/dashboard/ChartsSection.tsx
@@ -1,11 +1,11 @@
-import { ActiveUsersChart } from "@/components/cards/ActiveUsersChart";
+// import { ActiveUsersChart } from "@/components/cards/ActiveUsersChart";
 import { PatientTimelineChart } from "@/components/cards/PatientTimelineChart";
 // import { SalesOverviewChart } from "@/components/cards/SalesOverviewChart";
 import { memo } from "react";
 
 export const ChartsSection = memo(() => (
   <div className="flex flex-wrap mt-6 -mx-3">
-    <ActiveUsersChart />
+    {/* <ActiveUsersChart /> */}
     {/* <SalesOverviewChart /> */}
     <PatientTimelineChart />
   </div>
